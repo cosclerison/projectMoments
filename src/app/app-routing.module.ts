b8bf9b747a628e9d40a4ccbbe5888ff65a53e183
Clerison <cosclerison@gmail.com>
2022-09-29 12:27:56 -0300

    CSS nos componentes de layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 
@@ -7,6 +8,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'moments/new', component: NewMomentComponent},
 ];
 
 @NgModule({

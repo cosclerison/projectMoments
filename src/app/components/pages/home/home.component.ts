d37fb470020d5e40fa6f4c5332250bdb1c13a228
Clerison <cosclerison@gmail.com>
2022-09-30 11:52:33 -0300

    Resgatando dados do banco

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MomentService } from 'src/app/services/moment.service';
+import { Moments } from 'src/app/Moments';
+import { environment } from 'src/environments/environment';
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  // para buscar todos registros do banco
+  allMoments: Moments[] =[]
+  // para buscar momentos na pesquisa
+  moments: Moments[] = []
+  // Para buscar dados da api
+  baseApiUrl = environment.baseApiUrl
 
-  constructor() { }
+  // todo  search
+
+  constructor(
+    private momentService: MomentService,
+  ) { }
 
   ngOnInit(): void {
+    /* acessa o serviço momentService, no método getMoments, e detalha ele com subscribe
+      colocando o valor dentro de (items)
+    */
+    this.momentService.getMoments().subscribe((items) => {
+      // cria constante data e incorpora a formatação 'pt-BR' em uma nova data - um novo corpo de dados
+      const data = items.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
+      });
+
+      // referencia a contante data para os ambos setem o uso da data formatada com a região citada
+      this.allMoments = data;
+      this.moments = data;
+
+    })
   }
 
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Response } from '../Response';
+import { Moments } from '../Moments';
 import { Observable } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -15,6 +16,10 @@ export class MomentService {
   constructor(
     private http: HttpClient
   ) { }
+
+  getMoments(): Observable<Response<Moments[]>> {
+    return this.http.get<Response<Moments[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
